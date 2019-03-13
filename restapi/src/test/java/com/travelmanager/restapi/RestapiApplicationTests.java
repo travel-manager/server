38f1002babd703c004b929431991b53ad263f92b
Lloyd van Zaalen <lfb0801@gmail.com>
2019-03-13 11:14:28 +0100

    removed unwanted test

--- restapi/src/test/java/com/travelmanager/restapi/RestapiApplicationTests.java
+++ restapi/src/test/java/com/travelmanager/restapi/RestapiApplicationTests.java
@@ -8,10 +8,5 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class RestapiApplicationTests {
-
-    @Test
-    public void contextLoads() {
-    }
-
 }
 
