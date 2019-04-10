2e25d17c39d5af99242cf4dbbfa454f984d41e42
Lloyd van Zaalen <lfb0801@gmail.com>
2019-04-10 13:26:28 +0200

    Revert "Feature/transaction/37"

--- domain/src/main/java/interfaces/logic/handlers/ITransactionHandler.java
+++ domain/src/main/java/interfaces/logic/handlers/ITransactionHandler.java
@@ -3,7 +3,5 @@ package interfaces.logic.handlers;
 import interfaces.ICRUD;
 import models.Transaction;
 
-import java.util.List;
-
 public interface ITransactionHandler extends ICRUD<Transaction, Integer> {
 }
