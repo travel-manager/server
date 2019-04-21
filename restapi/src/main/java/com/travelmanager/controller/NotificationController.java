93224fe2f11ed9ac92204ae20f6d5b7d663186fb
Robin <>
2019-04-21 14:42:31 +0200

    notification endpoints added

--- restapi/src/main/java/com/travelmanager/controller/NotificationController.java
+++ restapi/src/main/java/com/travelmanager/controller/NotificationController.java
@@ -1,17 +1,24 @@
 package com.travelmanager.controller;
 
+import com.google.gson.Gson;
 import com.travelmanager.hateoas.abstracts.HateoasController;
 import com.travelmanager.hateoas.abstracts.HateoasService;
 import com.travelmanager.models.Notification;
 import com.travelmanager.services.NotificationService;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/notifications")
 public class NotificationController extends HateoasController<Notification, Integer> {
+
+    Gson json = new Gson();
 
     @Setter
     private NotificationService service;
@@ -24,5 +31,19 @@ public class NotificationController extends HateoasController<Notification, Inte
     @Override
     public Class<? extends HateoasController<Notification, Integer>> getClazz() {
         return this.getClass();
+    }
+
+    @RequestMapping(value = "/getallbytrip", method = RequestMethod.POST)
+    public ResponseEntity<String> getAllNotificationsByTripId(@RequestParam(name = "tripid") int tripId){
+        return new ResponseEntity<String>(json.toJson(service.getAllNotificationsByTripId(tripId)), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/deleteallbytrip", method = RequestMethod.POST)
+    public ResponseEntity<Boolean> deleteAllNotificationsByTripId(@RequestParam(name = "tripid") int tripId){
+        if(service.deleteAllNotificationsByTripId(tripId)){
+            return new ResponseEntity<Boolean>(true, HttpStatus.OK);
+        }else{
+            return new ResponseEntity<Boolean>(false, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
