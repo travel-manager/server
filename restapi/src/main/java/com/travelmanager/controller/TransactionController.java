2d546eb890788dba7c1dbdcae27e53c5fc8b0a24
Robin <>
2019-04-21 14:27:47 +0200

    transaction endpoints added

--- restapi/src/main/java/com/travelmanager/controller/TransactionController.java
+++ restapi/src/main/java/com/travelmanager/controller/TransactionController.java
@@ -1,5 +1,6 @@
 package com.travelmanager.controller;
 
+import com.google.gson.Gson;
 import com.travelmanager.enums.Gender;
 import com.travelmanager.hateoas.abstracts.HateoasController;
 import com.travelmanager.hateoas.abstracts.HateoasService;
@@ -8,6 +9,7 @@ import com.travelmanager.models.Traveller;
 import com.travelmanager.services.TransactionService;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,6 +19,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/transaction")
 public class TransactionController extends HateoasController<Transaction, Integer> {
+
+    Gson json = new Gson();
 
     @Setter
     private TransactionService service;
@@ -30,4 +34,15 @@ public class TransactionController extends HateoasController<Transaction, Intege
     public Class<? extends HateoasController<Transaction, Integer>> getClazz() {
         return this.getClass();
     }
+
+    @RequestMapping(value = "/getallbypayerandtripid", method = RequestMethod.POST)
+    public ResponseEntity<String> getAllByPayerAndTripId(@RequestParam(name = "payer") String payer, @RequestParam(name = "tripId") int tripId){
+        return new ResponseEntity<String>(json.toJson(service.getAllByPayerAndTripId(payer,tripId)), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/getallbypayerandtripid", method = RequestMethod.POST)
+    public ResponseEntity<String> getAllByFreeLoaderAndTripId(@RequestParam(name = "freeloader") String freeLoader, @RequestParam(name = "tripId") int tripId){
+        return new ResponseEntity<String>(json.toJson(service.getAllByFreeLoaderAndTripId(freeLoader,tripId)), HttpStatus.OK);
+    }
+
 }
